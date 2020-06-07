e9453a731fa0ab3e7aab495a0a49f870b826c443
Kalashnyk Bohdan <space.bohdan@gmail.com>
2020-06-07 19:19:19 +0300

    test1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const image = React.createElement('img', {
-  src:
-    'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640',
-  alt: 'Tacos With Lime',
-  width: 640,
-});
+// const image = React.createElement('img', {
+//   src:
+//     'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640',
+//   alt: 'Tacos With Lime',
+//   width: 640,
+// });
 const title = React.createElement('h2', null, 'Tacos With Lime');
 const price = React.createElement('p', null, 'Price: 10.99$');
 const button = React.createElement('button', { type: 'button' }, 'Add to cart');
@@ -18,7 +18,7 @@ const button = React.createElement('button', { type: 'button' }, 'Add to cart');
  * Обратите внимание на то, что свойство children это массив.
  */
 const product = React.createElement('div', {
-  children: [image, title, price, button],
+  children: [title, price, button],
 });
 
 ReactDOM.render(product, document.getElementById('root'));
