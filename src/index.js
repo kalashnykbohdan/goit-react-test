d58b372af29e1a3f1a52ce2110ee38988bfcd36d
Kalashnyk Bohdan <space.bohdan@gmail.com>
2020-06-07 19:10:37 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const image = React.createElement('img', {
+  src:
+    'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640',
+  alt: 'Tacos With Lime',
+  width: 640,
+});
+const title = React.createElement('h2', null, 'Tacos With Lime');
+const price = React.createElement('p', null, 'Price: 10.99$');
+const button = React.createElement('button', { type: 'button' }, 'Add to cart');
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// const product = React.createElement('div', null, image, title, price, button);
+
+/*
+ * Для передачи детей также используется свойство children параметра props.
+ * Обратите внимание на то, что свойство children это массив.
+ */
+const product = React.createElement('div', {
+  children: [image, title, price, button],
+});
+
+ReactDOM.render(product, document.getElementById('root'));
