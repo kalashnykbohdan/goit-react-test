1536071dce9bd70f879120643e4d11a4c10ff145
Kalashnyk Bohdan <space.bohdan@gmail.com>
2020-06-07 19:12:21 +0300

    Updates

---
+++ precache-manifest.789dc6628d9ee70b981d6278da4577ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc0773b9d48b46f7b4c3759068ba0c5f",
+    "url": "/kalashnykbohdan/goit-react-test/index.html"
+  },
+  {
+    "revision": "7cf882780c3dad9cffb4",
+    "url": "/kalashnykbohdan/goit-react-test/static/js/2.73953ea2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kalashnykbohdan/goit-react-test/static/js/2.73953ea2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bbabb294f9356c7358e",
+    "url": "/kalashnykbohdan/goit-react-test/static/js/main.cf0115dc.chunk.js"
+  },
+  {
+    "revision": "7e1ee8090a2d35e33e3b",
+    "url": "/kalashnykbohdan/goit-react-test/static/js/runtime-main.bc1ab0e6.js"
+  }
+]);
